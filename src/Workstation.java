1780e36f6289863ff12a50a8dc22bd538df16fa6
Britney <bakebr75@gmail.com>
2021-04-01 08:54:59 -0400

    added some more spicy in workstation

--- src/Workstation.java
+++ src/Workstation.java
@@ -1,10 +1,33 @@
+import java.util.ArrayList;
+
 public class Workstation implements Runnable{
+
     private int id;
+    private ArrayList<Component> C1Buffer;
+    private ArrayList<Component> C2Buffer;
+    private ArrayList<Component> C3Buffer;
 
     public Workstation(int id){
+
+        this.id = id;
+        C1Buffer = new ArrayList<Component>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
         this.id = id;
     }
 
+    public ArrayList<Component> getC1Buffer() {
+        return C1Buffer;
+    }
+
+    public void setC1Buffer(ArrayList<Component> C1Buffer) {
+        this.C1Buffer = C1Buffer;
+    }
 
     /**
      * Adds component to the queue. Is thread safe, no protection needed
